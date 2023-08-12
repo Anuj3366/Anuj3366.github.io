7116e08721447b647417a7dea42efcd9b6fe66ab
Anuj Garg <anujgarg3366@gmail.com>
2023-08-12 18:04:02 +0530

    introduction left done

---
+++ JS/index.js
@@ -0,0 +1,14 @@
+var typeData = new Typed(".role", {
+    strings: [
+      "Full Stack Developer",
+      "Coder",
+      "Dsa Lover",
+      "Backend Developer",
+      "Student",
+      "Gamer",
+    ],
+    loop: true,
+    typeSpeed: 110,
+    backSpeed: 10,
+  });
+  
